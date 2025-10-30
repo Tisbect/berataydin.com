e7b1d5ffb33564fb90a5b9598c1339ad92d888d2
Berat Aydın <aydinberat352@gmail.com>
2025-10-31 00:30:29 +0300

    Update: countdown date

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 1, 2026 00:00:00").getTime();
+var countDownDate = new Date("Jan 1, 2027 00:00:00").getTime();
 
 var x = setInterval(function() {
     var now = new Date().getTime();
